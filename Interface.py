b90c823693edef742e8bfd98cf8e0261a6223eca
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2022-06-13 14:20:24 +0200

    maj version ope 1.1

--- Interface.py
+++ Interface.py
@@ -1,51 +1,21 @@
 from enum import auto
+from lib2to3.pgen2.token import LEFTSHIFT
 from re import match
 from tkinter import *
 import sqlite3
-from turtle import bgcolor, pos
+from tkinter.font import BOLD
+from turtle import bgcolor, pos, position
 from PIL import ImageTk, Image
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-data_Nouvelle_Aquitaine = [[16, 'Charente', 352015, 5956.0],
-                           [17, 'Charente-Maritime', 651358, 6863.8],
-                           [19, 'Corrèze', 240073, 5856.8],
-                           [23, 'Creuse', 116617, 5565.4],
-                           [24, 'Dordogne', 413223, 9060.0],
-                           [33, 'Gironde', 1623749, 9975.6],
-                           [40, 'Landes', 413690, 9242.6],
-                           [47, 'Lot-et-Garonne', 331271, 5360.9],
-                           [64, 'Pyrénées-Atlantiques', 682621, 7644.8],
-                           [79, 'Deux-Sèvres', 374878, 5999.4],
-                           [86, 'Vienne', 438435, 6990.4],
-                           [87, 'Haute-Vienne', 372359, 5520.1]]
-
-df = pd.DataFrame(data_Nouvelle_Aquitaine, columns=[
-                  'Dept', 'Region', 'Pop', 'Superficie'])
-
-# Permet de créer le graphique matpotlib
-
-
-def pie():
-    data = data_Nouvelle_Aquitaine
-    colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'pink', 'turquoise',
-              'purple', 'orange', 'gray', 'white']
-    plt.title('Pourcentages populations départements Nouvelle-Aquitaine')
-    plt.pie([d[2] for d in data],
-            labels=[d[1] + ' (' + str(d[0]) + ')' for d in data],
-            colors=colors,
-            autopct='%1.1f %%',
-            shadow=False,
-            startangle=90)
-    plt.axis('equal')
-    plt.savefig("Image/figure.png")
 
 # Permet de savoir quelle action veut l'utilisateur
 
 
 def choix_action():
     choix = int(choice.get())
+    choice.delete(0)
     match choix:
         case 1:
             Action1()
@@ -67,6 +37,8 @@ def Action2():
 
 # Initialisation de l'interface Tkinter
 window = Tk()
+window.minsize(width=1600, height=900)
+window.maxsize(width=1600, height=900)
 window.configure(bg="#ffe599")
 window.iconbitmap('Image/logo.ico')
 window.geometry("1920x1080")
@@ -78,7 +50,7 @@ img3 = PhotoImage(file="Image/modele.png")
 
 # Titre de la fenêtre
 my_label = Label(text="Bienvenue", bg="#ffe599", fg="Black",
-                 font=("Arial", 40))  # setting up the labels
+                 font=("FARRAY", 40))  # setting up the labels
 my_label.pack()
 
 # Création de la MenuBar
@@ -107,22 +79,22 @@ imagelabel.place(x=1000, y=150)
 
 
 label = Label(text="Liste of commands : ")
-label.place(x=30, y=30)
+label.place(x=30, y=55)
 label.config(padx=0)
 
 label1 = Label(text="1. Print PIB of countries where you are")
-label1.place(x=45, y=50)
+label1.place(x=45, y=70)
 label1.config(padx=0)
 
 label2 = Label(text="2. Print emissions of countries where you are")
-label2.place(x=45, y=70)
-label.config(padx=0)
+label2.place(x=45, y=90)
+label2.config(padx=0)
 
 choice = Entry(text="")
-choice.place(x=45, y=100)
+choice.place(x=45, y=120)
 
 button = Button(text="Print", command=choix_action)
-button.place(x=55, y=150)
+button.place(x=45, y=150, width=125)
 
 # Lancement de l'interface graphique
 window.mainloop()
