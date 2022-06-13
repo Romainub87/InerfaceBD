fa0c94776212cc5fe854c88829352a3f7a0ccf6f
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2022-06-13 09:56:42 +0200

    maj

--- Interface.py
+++ Interface.py
@@ -1,2 +1,19 @@
 from tkinter import *
 import sqlite3
+
+window = Tk()
+window.geometry("1920x1080")
+
+label = Label(text="Liste of commands : ")
+label.place(x=30, y=30)
+label.config(padx=0)
+
+label1 = Label(text="1. Print PIB of countries where you are")
+label1.place(x=45, y=50)
+label1.config(padx=0)
+
+label2 = Label(text="2. Print emissions of countries where you are")
+label2.place(x=45, y=70)
+
+
+window.mainloop()
